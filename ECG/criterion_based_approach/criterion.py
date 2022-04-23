55c623a3af49e06e84174bf970e5b75d57f1cb8a
Dmitrii <ddm157@yandex.ru>
2022-04-23 22:53:15 +0300

    Add core functions

---
+++ ECG/criterion_based_approach/criterion.py
@@ -0,0 +1,11 @@
+from util import mV_to_mm
+
+def calculate_stemi_criterion(qtc_ms, median_r_amplitude, median_ste60):
+    ste60_mm = mV_to_mm(median_ste60)
+    r_amplitude_mm = mV_to_mm(median_r_amplitude)
+
+    return (1.196 * ste60_mm) + (0.059 * qtc_ms) - (0.326 * min(r_amplitude_mm, 15))
+
+
+def get_stemi_diagnosis(stemi_criterion, threshold=28.13):
+    return stemi_criterion > threshold
---
+++ ECG/criterion_based_approach/r_amplitude.py
@@ -0,0 +1,9 @@
+import numpy as np
+from util import get_channel, get_values_ignoring_nan
+
+def get_median_r_amplitude(ecg_signal, sampling_rate, ecg_parameters):
+    ecg_signal_v4, _ = get_channel(ecg_signal, sampling_rate, 'V4')
+
+    r_amplitudes = np.abs(ecg_signal_v4[ecg_parameters['R_peaks']] - get_values_ignoring_nan(ecg_signal_v4, ecg_parameters['P_offsets']))
+    
+    return np.nanmedian(r_amplitudes)
