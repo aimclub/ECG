4f447481afe0d7e3fa5323d777483c879e9b84fc
Dmitrii <ddm157@yandex.ru>
2022-04-30 22:24:48 +0300

    Fix threshold

--- ECG/criterion_based_approach/criterion.py
+++ ECG/criterion_based_approach/criterion.py
@@ -7,5 +7,5 @@ def calculate_stemi_criterion(qtc_ms, median_r_amplitude, median_ste60):
     return (1.0 * ste60_mm) + (0.06 * qtc_ms) - (0.5 * min(r_amplitude_mm, 10.5))
 
 
-def get_stemi_diagnosis(stemi_criterion, threshold=28.13):
+def get_stemi_diagnosis(stemi_criterion, threshold=27.1):
     return stemi_criterion > threshold
