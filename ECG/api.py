Fixed formula in tests

ECG/api.py
@@ -31,7 +31,7 @@ def diagnose_with_STEMI(signal: np.ndarray, sampling_rate: int) -> Tuple[Diagnos
 
     diagnosis_enum = Diagnosis.MI if stemi_diagnosis else Diagnosis.BER
     explanation = 'Criterion value calculated as follows: ' + \
-        '(1.196 * [STE60 V3 in mm]) + (0.059 * [QTc in ms]) - (0.326 * min([RA V4 in mm], 15)) = ' + str(stemi_criterion) + \
+        '(1.0 * [STE60 V3 in mm]) + (0.06 * [QTc in ms]) - (0.5 * min([RA V4 in mm], 10.5)) = ' + str(stemi_criterion) + \
         (' exceeded ', ' did not exceed ')[stemi_diagnosis] + \
         'the threshold 27.1, therefore the diagnosis is ' + diagnosis_enum.value
 