54e66d9ca9e6bce2c6ad41486aa895e716b66eaf
Dmitrii <ddm157@yandex.ru>
2022-04-30 22:22:57 +0300

    Adjust criterion after parameter tuning and cross-validation

--- ECG/criterion_based_approach/criterion.py
+++ ECG/criterion_based_approach/criterion.py
@@ -4,7 +4,7 @@ def calculate_stemi_criterion(qtc_ms, median_r_amplitude, median_ste60):
     ste60_mm = mV_to_mm(median_ste60)
     r_amplitude_mm = mV_to_mm(median_r_amplitude)
 
-    return (1.196 * ste60_mm) + (0.059 * qtc_ms) - (0.326 * min(r_amplitude_mm, 15))
+    return (1.0 * ste60_mm) + (0.06 * qtc_ms) - (0.5 * min(r_amplitude_mm, 10.5))
 
 
 def get_stemi_diagnosis(stemi_criterion, threshold=28.13):
