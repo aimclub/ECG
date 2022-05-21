b56dcd3722ac3b25e98c69571b73de1a556e55fa
Dmitrii <ddm157@yandex.ru>
2022-05-21 20:49:00 +0300

    Adjust criterion after 3-fold cv

--- ECG/criterion_based_approach/criterion.py
+++ ECG/criterion_based_approach/criterion.py
@@ -1,11 +1,12 @@
 from ECG.criterion_based_approach.util import mV_to_mm
+import numpy as np
 
 def calculate_stemi_criterion(qtc_ms, median_r_amplitude, median_ste60):
     ste60_mm = mV_to_mm(median_ste60)
     r_amplitude_mm = mV_to_mm(median_r_amplitude)
 
-    return (1.0 * ste60_mm) + (0.06 * qtc_ms) - (0.5 * min(r_amplitude_mm, 10.5))
+    return (2.9 * (ste60_mm - 3.2)) + (0.3 * (qtc_ms - 458.3)) + (-1.7 * (np.minimum(r_amplitude_mm, 19) - 14.1))
 
 
-def get_stemi_diagnosis(stemi_criterion, threshold=27.1):
-    return stemi_criterion > threshold
+def get_stemi_diagnosis(stemi_criterion, threshold=4.1):
+    return stemi_criterion >= threshold
