d7d734d0b7b0ae6a63c9c1dda94826ed46fa42bb
tanyapole <tanusha2406@gmail.com>
2022-09-30 19:33:58 +0300

    update dosctrings in api

--- ECG/api.py
+++ ECG/api.py
@@ -18,7 +18,7 @@ def convert_image_to_signal(image: Image.Image) -> np.ndarray or Failed:
     of the signal.
 
     Args:
-        image (Image.Image): input image with ECG signal
+        image (Image.Image): input image with ECG signal (one lead; see readme for the image requirements)
 
     Returns:
         np.ndarray or Failed: array representation of ECG signal or Failed
@@ -43,8 +43,8 @@ def check_ST_elevation(signal: np.ndarray, sampling_rate: int)\
     """This function checks for significant ST elevation using classic CV methods.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
-        sampling_rate (int): sampling rate
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
+        sampling_rate (int): sampling rate (see readme for the requirements)
 
     Returns:
         Tuple[ElevatedST, TextExplanation] or Failed: a tuple of ST elevation evaluation
@@ -73,7 +73,7 @@ def check_ST_elevation_with_NN(signal: np.ndarray)\
     """This function checks for significant ST elevation using a NN.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
 
     Returns:
         Tuple[ElevatedST, TextAndImageExplanation] or Failed:
@@ -98,8 +98,8 @@ def evaluate_risk_markers(signal: np.ndarray, sampling_rate: int)\
     """This function evaluates MI risk markers.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
-        sampling_rate (int): sampling rate
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
+        sampling_rate (int): sampling rate (see readme for the requirements)
 
     Returns:
         RiskMarkers or Failed: MI risk markers values or Failed
@@ -123,8 +123,8 @@ def diagnose_with_risk_markers(signal: np.ndarray,
     that contains either of two. Uses formula based on MI risk marker values.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
-        sampling_rate (int): sampling rate
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
+        sampling_rate (int): sampling rate (see readme for the requirements)
         tuned (bool, optional): True if you want to use formula tuned od CPSC 2018,
             otherwise False. Default is False
 
@@ -162,7 +162,7 @@ def check_BER_with_NN(signal: np.ndarray)\
     """This functions checks for BER on ECG signal with ST elevation. Uses a NN.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
 
     Returns:
         Tuple[bool, TextAndImageExplanation] or Failed: a tuple containing boolean
@@ -181,7 +181,7 @@ def check_MI_with_NN(signal: np.ndarray) -> Tuple[bool, TextExplanation] or Fail
     """This functions checks for MI on ECG signal with ST elevation. Uses a NN.
 
     Args:
-        signal (np.ndarray): array representation of ECG signal
+        signal (np.ndarray): array representation of ECG signal (contains 12 rows, i-th row for i-th lead)
 
     Returns:
         Tuple[bool, TextAndImageExplanation] or Failed: a tuple containing boolean
